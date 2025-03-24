094e12eb96f4f06a7ef327310fea5cc79f3ca657
hanifmmufid <hanif.m.mufid@gmail.com>
2025-03-24 10:08:24 +0700

    add theme and sheet

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,34 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
-import { Button } from "@/components/ui/button";
-import { ShoppingCart, UserIcon } from "lucide-react";
+import Menu from "./menu";
 
 const Header = () => {
-    return ( 
-        <header className="w-full border-b">
-            <div className="wrapper flex-between">
-                <div className="flex-start">
-                    <Link href="/" className="flex-start">
-                        <Image src='/images/logo.png' alt={`${APP_NAME} logo`} height={48} width={48} priority={true}/>
-                    </Link>
-                    <span className="hidden lg:block font-bold text-2x1 ml-3 uppercase">{APP_NAME}</span>
-                </div>
-                <div className="space-x-2">
-                        <Button asChild variant='ghost'>
-                            <Link href="/cart">
-                                <ShoppingCart/> Cart
-                            </Link>
-                        </Button>
-                        <Button asChild variant='ghost'>
-                            <Link href="/sign-in">
-                                <UserIcon/> Sign In 
-                            </Link>
-                        </Button>
-                </div>
-            </div>
-        </header>
-     );
-}
- 
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.png"
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+          </Link>
+          <span className="hidden lg:block font-bold text-2x1 ml-3 uppercase">
+            {APP_NAME}
+          </span>
+        </div>
+        <Menu />
+      </div>
+    </header>
+  );
+};
+
 export default Header;
